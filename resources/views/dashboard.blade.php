f76ee6787166b6f25ea98d77c239def59a6fe87e
Lucas Lins <lucas.fsl.92@gmail.com>
2024-04-03 02:07:18 +0000

    HEY-16: refactor create question view, using blade components

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,17 @@
 <x-app-layout>
+    
     <x-slot name="header">
-        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+        <x-header>
             {{ __('Dashboard') }}
-        </h2>
+        </x-header>
     </x-slot>
-
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <x-questions.form post :action="route('question.store')">
-                <x-questions.text-area label="Question" name="questions"></x-questions>
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                    Save
-                </button>
-                <button type="reset" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                    Cancel
-                </button>
-            </x-form>
-        </div>
-    </div>
+    
+    <x-questions.container>
+        <x-questions.form post :action="route('question.store')">
+            <x-questions.text-area label="Question" name="question"></x-questions>
+            <x-questions.submit-button>Save</x-questions>
+            <x-questions.reset-button>Cancel</x-questions>
+        </x-questions.form>
+    </x-questions.container>
+    
 </x-app-layout>
