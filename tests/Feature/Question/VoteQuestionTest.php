45a8448e2f8a682ac319ad03f6c9dcb9a565aecc
Lucas Lins <lucas.fsl.92@gmail.com>
2024-04-27 18:23:33 +0000

    HEY-17: creating testing for dislike flow

--- tests/Feature/Question/VoteQuestionTest.php
+++ tests/Feature/Question/VoteQuestionTest.php
@@ -22,13 +22,77 @@ it('should be possible like a question', function () {
 
     assertDatabaseHas('votes', [
         'question_id' => $question->id,
-        'question_id' => $user->id,
+        'user_id'     => $user->id,
         'rating'      => 1,
     ]);
 });
 
 # artisan test --filter "should not be able to like more than 1 time"
 it('should not be able to like more than 1 time', function () {
+    //Arrange
+    $user     = User::factory()->create();
+    $user2    = User::factory()->create();
+    $question = Question::factory()->create();
+
+    //Act
+    actingAs($user);
+
+    post(route("votes.like", $question->uuid), $question->toArray());
+    post(route("votes.like", $question->uuid), $question->toArray());
+    post(route("votes.like", $question->uuid), $question->toArray());
+    post(route("votes.like", $question->uuid), $question->toArray());
+
+    actingAs($user2);
+
+    post(route("votes.like", $question->uuid), $question->toArray());
+    post(route("votes.like", $question->uuid), $question->toArray());
+    //Assert
+
+    expect(
+        $user->votes()
+            ->where('question_id', '=', $question->id)
+            ->where('user_id', '=', $user->id)
+            ->get()
+    )->toHaveCount(1);
+
+    expect(
+        $user2->votes()
+            ->where('question_id', '=', $question->id)
+            ->where('user_id', '=', $user2->id)
+            ->get()
+    )->toHaveCount(1);
+
+    $question->votes()->each(function ($vote) {
+        dump($vote->toArray());
+    });
+
+    expect(
+        $question->votes()
+            ->get()
+    )->toHaveCount(2);
+});
+
+# artisan test --filter "should be possible dislike a question"
+it('should be possible dislike a question', function () {
+    //Arrange
+    $user     = User::factory()->create();
+    $question = Question::factory()->create();
+
+    //Act
+    actingAs($user);
+    post(route("votes.dislike", $question->uuid), $question->toArray());
+
+    //Assert
+
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'user_id'     => $user->id,
+        'rating'      => 0,
+    ]);
+});
+
+# artisan test --filter "should not be able to dislike more than 1 time"
+it('should not be able to dislike more than 1 time', function () {
     //Arrange
     $user     = User::factory()->create();
     $user2    = User::factory()->create();
